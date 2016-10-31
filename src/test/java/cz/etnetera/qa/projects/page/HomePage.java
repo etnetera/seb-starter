b3043fa766b7b86b02b47799e393533dca85ecbc
Petr Benes <petr.benes@etnetera.cz>
2016-10-31 22:15:33 +0100

    package fixes

--- src/test/java/cz/etnetera/qa/projects/page/HomePage.java
+++ src/test/java/cz/etnetera/qa/projects/page/HomePage.java
@@ -1,8 +1,15 @@
 package cz.etnetera.qa.projects.page;
 
+import cz.etnetera.seb.element.SebElement;
 import cz.etnetera.seb.page.Page;
 import cz.etnetera.seb.page.PageConfig;
+import org.openqa.selenium.support.FindBy;
 
 @PageConfig(uri = "/")
 public class HomePage extends Page {
+
+    @FindBy(id = "elementId")
+    protected SebElement someElement;
+
+
 }
